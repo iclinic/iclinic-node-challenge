29e51c5a827390b840c9f9cb653f2b6d6f8721e0
ederson <ederson@kiina.ai>
2021-04-27 14:43:58 -0300

    Server now uses port and host defined on process.env or value on default config

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
-const port = process.env.PORT || 5000;
-const host = process.env.HOST || '0.0.0.0' 
-
+const config = require('config');
 const http = require('http');
+
+
+
 const app = require('./app');
 const server = http.createServer(app);
 
+const port = process.env.PORT || config.get('port');
+const host = process.env.HOST || config.get('host');
 server.listen(port, host, () => {
     console.log(`listening on port ${port}...`);
 });
+
+module.exports = server;
