1662a619f178dc02cb54bbb62ef5c4623e199803
ederson <ederson@kiina.ai>
2021-04-27 19:02:40 -0300

    Added 2 new params for the mongoDB connection to avoid depreciation problems

--- db/index.js
+++ db/index.js
@@ -4,7 +4,7 @@ const config = require('config');
 module.exports = async function() {
   const targetDB = config.get('db');
   try {
-    const db = await mongoose.connect(targetDB);
+    const db = await mongoose.connect(targetDB, {useNewUrlParser: true, useUnifiedTopology: true });
   } catch (error) {
     throw new Error(error);
   } 
